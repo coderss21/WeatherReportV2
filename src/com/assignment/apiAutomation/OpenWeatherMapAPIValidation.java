424bdc54d86c37ac96c6677765671d6d3bf56791
Shubhra <shubhrasingha2015@gmail.com>
2020-12-27 14:47:29 +0530

    get temp in degree method is added

--- src/com/assignment/apiAutomation/OpenWeatherMapAPIValidation.java
+++ src/com/assignment/apiAutomation/OpenWeatherMapAPIValidation.java
@@ -2,7 +2,6 @@ package com.assignment.apiAutomation;
 
 import io.restassured.RestAssured;
 import io.restassured.path.json.JsonPath;
-import io.restassured.response.ValidatableResponse;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
@@ -12,24 +11,28 @@ import static io.restassured.RestAssured.given;
 
 public class OpenWeatherMapAPIValidation {
     public String apiKey="7fe67bf08c80ded756e598d6f8fedaea";
+    private static String temp;
+    private String city;
 
     @Parameters({"URL1","cityName","countryCode","units"})
     @Test
     public void testWeatherAPI(String URL,String cityName, String countryCode,String units){
         RestAssured.baseURI =URL;
-       String getResponse= given().log().all().queryParam("appid",apiKey).
+       String getResponse= given().queryParam("appid",apiKey).
                 queryParam("q",cityName,countryCode).
-                queryParam("units",units).when().get().then().assertThat().log().all()
+                queryParam("units",units).when().get().then().assertThat()
                 .statusCode(200).extract().response().asString();
 
           JsonPath js = new JsonPath(getResponse);
-          String city= js.getString("name");
-          String tempInDegree = js.getString("main.temp");
+          city= js.getString("name");
+          temp = js.getString("main.temp");
+          System.out.println(city);
+          System.out.println(temp);
+    }
 
-        System.out.println(city);
-        System.out.println(tempInDegree);
 
-
+    public String getTemp(){
+        return temp;
     }
 
 
